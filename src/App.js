c18a2bfa6b2fa473fe585547dddcfa66fcaac89b
Carl-W <carl.igelstroem@tiket.com>
2019-12-04 19:56:08 +0800

    init reactive-qr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import ReactiveQR from "reactive-qr";
+
+const CameraContainer = styled.div`
+  width: 300;
+  height: 300;
+`;
 
 function App() {
-  return <div>Hello from react!</div>;
+  return (
+    <>
+      Hello from react!
+      <CameraContainer>
+        <ReactiveQR onCode={console.log} />
+      </CameraContainer>
+    </>
+  );
 }
 
 export default App;

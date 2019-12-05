32d201b4bd320d777abd4666bc43ff1e4e2a0a4e
Carl-W <carl.igelstroem@tiket.com>
2019-12-05 09:28:23 +0800

    scanning successful

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
+import QrReader from "react-qr-reader";
 
-function App() {
-  return <div>Hello from react!</div>;
+const Container = styled.div`
+  width: 350;
+  height: 350;
+`;
+
+class App extends Component {
+  state = {
+    scanResult: "No result"
+  };
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        scanResult: data
+      });
+    }
+  };
+
+  handleError = err => {
+    console.log(err);
+  };
+
+  render() {
+    return (
+      <Container>
+        <QrReader
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: 300, height: 300 }}
+        />
+        <p>{this.state.scanResult}</p>
+      </Container>
+    );
+  }
 }
 
 export default App;
